671772eca47b7627fa766e0d6f49c4fddb94b889
Anish Nakhale <anishwork26@gmail.com>
2025-03-23 12:51:09 +0530

    Positions in css

---
+++ src/components/Position.tsx
@@ -0,0 +1,31 @@
+function Position() {
+  return (
+    <div className="bg-yellow-500 my-[32px] h-[720px] w-[720px]">
+      <div className="relative bg-gray-200 p-4">
+        <div className="static bg-blue-500 text-white p-2">Static Position</div>
+      </div>
+      <div className="relative bg-gray-200 p-4 my-[4px]">
+        <div className="relative top-5 left-5  bg-red-500 text-white p-2">
+          Relative Position
+        </div>
+      </div>
+      <div className="relative bg-gray-200 p-4 h-40 my-[4px]">
+        <div className="absolute top-2 right-2 bg-green-500 text-white p-2">
+          Absolute Position
+        </div>
+      </div>
+      <div className="fixed bottom-0 right-0 bg-yellow-500 text-white p-2">
+        Fixed Position
+      </div>
+      <div className="h-60 overflow-y-scroll bg-gray-200 p-4 my-[4px]">
+        <div className="sticky top-0 bg-purple-500 text-white p-2">
+          Sticky Position
+        </div>
+        <p className="mt-10">Scroll down to see the sticky effect.</p>
+        <div className="h-80 bg-gray-400"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Position;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
+import Position from "../components/Position";
 import Slider from "../components/Slider";
 
 function Home() {
   return (
     <>
-      <div className="h-[100vh] w-full bg-gray-700">
+      <div className="h-full w-full bg-gray-700">
         <Navbar />
         <div className="heroSection p-[64px]">
           <Slider />
+          <Position/>
         </div>
       </div>
     </>
